93a23b4c5cb1cc851307b3fcd57f4e8f5d06eb46
Omar Lopez <l21530306@cancun.tecnm.mx>
2025-04-07 17:32:00 -0500

    inicio

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.faq-question').forEach(question => {
+    question.addEventListener('click', () => {
+      question.classList.toggle('active');
+      const answer = question.nextElementSibling;
+      answer.style.maxHeight = answer.style.maxHeight ? null : answer.scrollHeight + 'px';
+    });
+  });
